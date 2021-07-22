29895bda0a243c0f449d9f8180522e9bb24bd83d
anubis <schmidte@list.ru>
2021-07-22 12:25:14 +0300

    russian

---
+++ lang/ru/availability_enroldate.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings.
+ *
+ * @package availability_enroldate
+ * @copyright 2021 Sooraj Singh
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['ajaxerror'] = 'Ошибка соединения с сервером для преобразования времени';
+$string['direction_before'] = 'Дата зачисления';
+$string['enroldate_after'] = 'После';
+$string['direction_label'] = 'Направление';
+$string['enroldate_before'] = 'До';
+$string['description'] = 'Предотвращение доступа при зачислении после (или до) даты и времени.';
+$string['full_from'] = 'It is after <strong>{$a}</strong>';
+$string['full_from_date'] = 'It is on or after <strong>{$a}</strong>';
+$string['full_until'] = 'It is before <strong>{$a}</strong>';
+$string['full_until_date'] = 'It is before end of <strong>{$a}</strong>';
+$string['pluginname'] = 'Ограничение по дате зачисления';
+$string['short_from'] = 'Доступно, если зачислен после <strong>{$a}</strong>.';
+$string['short_from_date'] = 'Доступно, если зачислен после <strong>{$a}</strong>.';
+$string['short_until'] = 'Доступно, если зачислен до <strong>{$a}</strong>.';
+$string['short_until_date'] = 'Доступно, если зачислен до <strong>{$a}</strong>.';
+$string['title'] = 'Дата зачисления';
+$string['privacy:metadata'] = 'Плагин ограничения по дате не хранит никаких личных данных.';
